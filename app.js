21df4f29fb3e23e50781fe1717238be98afda2c5
bhavya2810 <bhavyadhiman7@gmail.com>
2017-11-23 20:18:22 +0530

    fifth commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var port = process.env.port;
-mongoose.connect('mongodb://localhost:'+process.env.port+'/chatMessenger');
+mongoose.connect('mongodb://chatmessenger:chatmessenger@ds119406.mlab.com:19406/chatmessenger');
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var path = require('path');
