aa5f39503143f978504b9c82f6a68e0006a8ab44
bhavya2810 <bhavyadhiman7@gmail.com>
2017-11-23 21:01:19 +0530

    hdd ho gyi

--- src/changeSettings.js
+++ src/changeSettings.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 function getDetails(){
     $.ajax({
-        url:'http://localhost:8080/getGeneralDetails',
+        url:'/getGeneralDetails',
         type:'GET',
         success:function(data){
            $("#firstName").val(data.firstName);
